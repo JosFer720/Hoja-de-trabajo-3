d2a12d1a9776cbce8be1cd7c8a131ce6471f0715
erickguerra2 <eagi578@gmail.com>
2024-01-30 19:16:55 -0600

    Gnome Sort

--- GnomeSort.java
+++ GnomeSort.java
@@ -1,3 +1,27 @@
-public class GnomeSort {
-    
+import java.lang.Comparable;
+
+public class GnomeSort <T extends Comparable<T>> implements IGenericSort<T> {
+
+    @Override
+    public T[] sort(T[] arr) {
+        int n = arr.length;
+        int index = 0;
+
+        while (index < n) {
+            if (index == 0 || arr[index - 1].compareTo(arr[index]) <= 0) {
+                index++;
+            } else {
+                swap(arr, index, index - 1);
+                index--;
+            }
+        }
+
+        return arr;
+    }
+
+    private static <T extends Comparable<T>> void swap(T[] arr, int i, int j) {
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
