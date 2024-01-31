24789329ab277ad822964d5adc8b95bea4d1c24e
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2024-01-30 19:53:04 -0600

    Creacion metodos para insertion sort

--- SortX.java
+++ SortX.java
@@ -1,3 +1,27 @@
-public class SortX {
-    
+import java.lang.Comparable;
+
+public class SortX<T extends Comparable<T>> implements IGenericSort<T> {
+
+    @Override
+    public T[] sort(T[] arr) {
+        if (arr == null) {
+            throw new IllegalArgumentException("Input array cannot be null");
+        }
+
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            T key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        return arr;
+    }
 }
